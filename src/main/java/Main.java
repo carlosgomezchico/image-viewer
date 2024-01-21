e095f1014906f4e834f2f411ef422cceb4530d39
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 13:43:58 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+package software.ulpgc.imageviewer;
+
+import java.io.File;
+
+public class Main {
+    public static final String root = "C:/Users/Entrar/Pictures";
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        ImageDisplay imageDisplay = frame.imageDisplay();
+        ImageLoader imageLoader = new FileImageLoader(new File(root));
+        Image initialImage = imageLoader.load();
+
+        frame.add("<", new PreviousImageCommand(imageDisplay));
+        frame.add(">", new NextImageCommand(imageDisplay));
+
+        imageDisplay.show(initialImage);
+        frame.setVisible(true);
+    }
 }
