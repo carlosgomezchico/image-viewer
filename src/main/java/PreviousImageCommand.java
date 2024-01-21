bf47d5e4eeda0f2bda026ad2cf54aa9fe534163d
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 13:45:34 +0000

    Update PreviousImageCommand.java

--- src/main/java/PreviousImageCommand.java
+++ src/main/java/PreviousImageCommand.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class PreviousImageCommand {
+package software.ulpgc.imageviewer;
+
+public class PreviousImageCommand implements Command {
+    private final ImageDisplay imageDisplay;
+
+    public PreviousImageCommand(ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+    }
+
+    @Override
+    public void execute() {
+        imageDisplay.show(imageDisplay.image().prev());
+    }
 }
