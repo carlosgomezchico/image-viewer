ec8f6cf9a43661f61b594161f35fd128cdea21a5
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 13:42:45 +0000

    Update SwingImageDisplay.java

--- src/main/java/SwingImageDisplay.java
+++ src/main/java/SwingImageDisplay.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class SwingImageDisplay {
+package software.ulpgc.imageviewer;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class SwingImageDisplay extends JPanel implements ImageDisplay {
+    private Image image;
+    private BufferedImage bitmap;
+
+    @Override
+    public void show(Image image) {
+        this.image = image;
+        this.bitmap = load(image.name());
+        this.repaint();
+    }
+
+    @Override
+    public Image image() {
+        return image;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        if (bitmap != null) {
+            Resizer resizer = new Resizer(new Dimension(this.getWidth(), this.getHeight()));
+            Dimension resized = resizer.resize(new Dimension(bitmap.getWidth(), bitmap.getHeight()));
+            int x = (this.getWidth() - resized.width) / 2;
+            int y = (this.getHeight() - resized.height) / 2;
+            g.drawImage(bitmap, x, y, resized.width, resized.height, null);
+        }
+    }
+
+    private BufferedImage load(String name) {
+        try {
+            return ImageIO.read(new File(name));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class Resizer {
+        private final Dimension dimension;
+
+        public Resizer(Dimension dimension) {
+            this.dimension = dimension;
+        }
+
+        public Dimension resize(Dimension imageDimension) {
+            double widthRatio = dimension.getWidth() / imageDimension.getWidth();
+            double heightRatio = dimension.getHeight() / imageDimension.getHeight();
+            double ratio = Math.min(widthRatio, heightRatio);
+            return new Dimension((int) (imageDimension.width * ratio), (int) (imageDimension.height * ratio));
+        }
+    }
 }
